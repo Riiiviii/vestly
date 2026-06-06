e39046fdeae33765fe33a61ff30614719f6efdbc
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-06-06 12:04:58 +1000

    feat: add logo styling and custom button component

--- client/src/components/layout/navbar.tsx
+++ client/src/components/layout/navbar.tsx
@@ -1,11 +1,13 @@
+import Button from '../ui/button'
+
 function Navbar() {
   return (
-    <nav className="flex justify-between items-center py-4 px-10 border-b border-(--border-primary) w-full">
-      <div className="flex items-center gap-2 rounded-6xl">
-        <span className="inline-block w-2 h-2 rounded-xs bg-(--glow-primary) rotate-45 shadow-[0_0_12px_rgba(90,220,150,0.7)]"></span>
+    <nav className="flex justify-between items-center py-4 px-8 border-b border-(--border-primary) w-full">
+      <Button variant="none" className="flex items-center gap-3">
+        <span className="inline-block w-3 h-3 rounded-xs bg-(--glow-primary) rotate-45 shadow-[0_0_12px_rgba(90,220,150,0.7)]"></span>
 
-        <h1 className="font-serif font-bold">V E S T L Y</h1>
-      </div>
+        <h1 className="font-serif text-2xl font-bold">V E S T L Y</h1>
+      </Button>
       <div>
         <div></div>
         <div></div>
