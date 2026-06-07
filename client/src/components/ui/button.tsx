8b22d7bcd65f17af8ac87c27682037fa07d0c55b
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-06-07 11:59:27 +1000

    feat: add styling variants for navbar

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -1,19 +1,26 @@
 import type { ComponentProps } from 'react'
 import { twMerge } from 'tailwind-merge'
 
-type Variant = 'primary' | 'secondary' | 'ghost-destructive' | 'none'
+type Variant = 'primary' | 'none' | 'navbar'
+type Active = true | false
 
 type ButtonProps = {
   variant?: Variant
+  active?: boolean
 } & ComponentProps<'button'>
 
-function Button({ variant = 'primary', className, ...props }: ButtonProps) {
+function Button({
+  variant = 'primary',
+  active = false,
+  className,
+  ...props
+}: ButtonProps) {
   return (
     <button
       {...props}
       className={twMerge(
-        `transition-colors px-2 py-1 `,
-        getVariantStyles(variant),
+        `transition-colors px-4 py-3 rounded-lg cursor-pointer`,
+        getVariantStyles(variant, active),
         className,
       )}
     />
@@ -22,17 +29,26 @@ function Button({ variant = 'primary', className, ...props }: ButtonProps) {
 
 export default Button
 
-function getVariantStyles(variant: Variant) {
+function getVariantStyles(variant: Variant, active: Active) {
+  if (variant === 'navbar') return getNavbarStyles(active)
+
   switch (variant) {
     case 'primary':
-      return 'bg-violet-600 hover:bg-violet-500'
-    case 'secondary':
-      return
-    case 'ghost-destructive':
-      return
+      return ''
     case 'none':
-      return
+      return ''
     default:
       throw new Error(`Invalid variant: ${variant satisfies never}`)
+  }
+}
+
+function getNavbarStyles(active: Active) {
+  switch (active) {
+    case true:
+      return 'text-(--brand-primary) bg-(--brand-secondary) cursor-default disabled:cursor-not-allowed'
+    case false:
+      return 'text-zinc-300 hover:text-white hover:bg-[#141a16]'
+    default:
+      throw new Error(`Invalid variant: ${active satisfies never}`)
   }
 }
