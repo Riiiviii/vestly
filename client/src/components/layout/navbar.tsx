52561fa008dfcd08c46cbbcc64bfb10821d5435e
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-06-07 11:59:43 +1000

    feat: add navbar links to navbar

--- client/src/components/layout/navbar.tsx
+++ client/src/components/layout/navbar.tsx
@@ -1,15 +1,32 @@
 import Button from '../ui/button'
 
+type NavLinks = {
+  href: string
+  label: string
+  active?: boolean
+}
+
+const navLinks: NavLinks[] = [
+  { href: '/', label: 'Analyze' },
+  { href: '/about', label: 'About' },
+]
+
 function Navbar() {
   return (
-    <nav className="flex justify-between items-center py-4 px-8 border-b border-(--border-primary) w-full">
+    <nav className="flex justify-between items-center py-4 px-8 border-b border-(--border-color) w-full">
       <Button variant="none" className="flex items-center gap-3">
-        <span className="inline-block w-3 h-3 rounded-xs bg-(--glow-primary) rotate-45 shadow-[0_0_12px_rgba(90,220,150,0.7)]"></span>
+        <span className="inline-block w-3 h-3 rounded-xs bg-(--brand-primary) rotate-45 shadow-[0_0_12px_rgba(90,220,150,0.7)]"></span>
 
         <h1 className="font-serif text-2xl font-bold">V E S T L Y</h1>
       </Button>
-      <div>
-        <div></div>
+      <div className="flex items-center">
+        <div className="flex gap-2">
+          {navLinks.map((link) => (
+            <Button variant="navbar" active={true}>
+              {link.label}
+            </Button>
+          ))}
+        </div>
         <div></div>
       </div>
     </nav>
