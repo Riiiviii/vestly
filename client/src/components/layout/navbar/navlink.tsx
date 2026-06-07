08651b2f2cdce0cf1c513c899d01ff6415c9c2d1
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-06-07 12:31:44 +1000

    feat: update styling for active and inactive links

--- client/src/components/layout/navbar/navlink.tsx
+++ client/src/components/layout/navbar/navlink.tsx
@@ -8,9 +8,17 @@ function NavLink({ className, ...props }: NavLinkProps) {
   return (
     <Link
       {...props}
-      className={twMerge('transition-colors px-4 py-3 rounded-lg cursor-pointer', className)}
-      activeProps={{ className: 'text-(--brand-primary) bg-(--brand-secondary) cursor-default' }}
-      inactiveProps={{ className: 'text-zinc-300 hover:text-white hover:bg-[#141a16]' }}
+      className={twMerge(
+        'transition-colors px-5 py-2.5 rounded-lg cursor-pointer font-semibold',
+        className,
+      )}
+      activeProps={{
+        className:
+          'text-(--brand-primary) bg-(--brand-secondary) cursor-default',
+      }}
+      inactiveProps={{
+        className: 'text-(--text-muted) hover:text-white hover:bg-[#141a16]',
+      }}
     />
   )
 }
