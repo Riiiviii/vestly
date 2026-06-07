4b018a25af09a25253d8e57506f99405cbf79214
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-06-07 12:42:51 +1000

    feat: add link to vestly logo

--- client/src/components/layout/navbar/navbar.tsx
+++ client/src/components/layout/navbar/navbar.tsx
@@ -1,4 +1,4 @@
-import Button from '../../ui/button'
+import { Link } from '@tanstack/react-router'
 import NavLink from './navlink'
 
 type NavLinks = {
@@ -13,12 +13,12 @@ const navLinks: NavLinks[] = [
 
 function Navbar() {
   return (
-    <nav className="flex justify-between items-center py-2.5 px-10 border-b border-(--border-color) w-full">
-      <Button variant="none" className="flex items-center gap-3">
-        <span className="inline-block w-3 h-3 rounded-xs bg-(--brand-primary) rotate-45 shadow-[0_0_12px_rgba(90,220,150,0.7)]"></span>
+    <nav className="flex justify-between items-center py-4 px-10 border-b border-(--border-color) w-full">
+      <Link to="/" className="flex items-center gap-3">
+        <span className="inline-block w-2.5 h-2.5 rounded-xs bg-(--brand-primary) rotate-45 shadow-[0_0_12px_rgba(90,220,150,0.7)]"></span>
 
-        <h1 className="font-serif text-2xl font-bold">V E S T L Y</h1>
-      </Button>
+        <h1 className="font-serif text-[24px] font-bold">V E S T L Y</h1>
+      </Link>
       <div className="flex items-center gap-5">
         <div className="flex gap-2">
           {navLinks.map((link) => (
