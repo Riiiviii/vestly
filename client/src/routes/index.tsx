991d2f95107e15a6b97953f051b7007a30394632
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-06-09 13:34:43 +1000

    feat: add hero component to home route

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,7 +1,12 @@
+import Hero from '#/components/landing/hero'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
-  return <div className="p-8"></div>
+  return (
+    <div className="flex-1 flex items-center justify-center w-full">
+      <Hero />
+    </div>
+  )
 }
