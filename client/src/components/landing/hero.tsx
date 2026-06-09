11c1610d0acf5728e91999a0d24e91c2f2411b17
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-06-09 13:34:26 +1000

    feat: add landing page hero component

---
+++ client/src/components/landing/hero.tsx
@@ -0,0 +1,15 @@
+function Hero() {
+  return (
+    <section className="flex flex-col gap-5 justify-center items-center min-h-max">
+      <h2 className="font-mono">STOCK ANALYSIS SIMPLIFIED</h2>
+      <h1>
+        Every Stock,
+        <span className="text-(--brand-primary)">
+          <i>deoded.</i>
+        </span>
+      </h1>
+    </section>
+  )
+}
+
+export default Hero
