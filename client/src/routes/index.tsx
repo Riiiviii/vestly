59b7db6dec314f1d8c9b5468bbeb12aff043bb4a
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-06-10 13:57:41 +1000

    fix: issue with page positioning

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -6,7 +6,7 @@ export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
   return (
-    <main className="relative flex-1 flex flex-col items-center justify-center w-full gap-12">
+    <main className="relative flex flex-col items-center justify-center w-full gap-12 flex-1">
       <div className="relative">
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-80 h-80 bg-(--brand-primary) opacity-[0.18] blur-[180px] rounded-full -z-10" />
         <Hero />
