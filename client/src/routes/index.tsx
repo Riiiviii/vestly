ba4e7a8683662035bbd29ad7d0cb1f0227007a29
Gabriel Riven Wahnich <riven.development@gmail.com>
2026-06-10 14:11:33 +1000

    feat: update home component to utilize ticker state and new component

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,17 +1,22 @@
 import Hero from '#/components/landing/hero'
-import InputTicker from '#/components/landing/input-ticker'
+import SampleTickers from '#/components/landing/sample-tickers'
+import TickerInput from '#/components/landing/ticker-input'
 import { createFileRoute } from '@tanstack/react-router'
+import { useState } from 'react'
 
 export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
+  const [ticker, setTicker] = useState('')
+
   return (
     <main className="relative flex flex-col items-center justify-center w-full gap-12 flex-1">
       <div className="relative">
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-80 h-80 bg-(--brand-primary) opacity-[0.18] blur-[180px] rounded-full -z-10" />
         <Hero />
       </div>
-      <InputTicker />
+      <TickerInput ticker={ticker} setTicker={setTicker} />
+      <SampleTickers setTicker={setTicker} />
     </main>
   )
 }
